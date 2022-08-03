c523b6083a97474a98fa82fdff85d8a04d30612b
Jack <dqt96760@126.com>
2022-08-03 22:00:22 +0800

    递归训练2.0

--- Python/pycharm2022/pythonProject1/test/section1.py
+++ Python/pycharm2022/pythonProject1/test/section1.py
@@ -1,25 +1,26 @@
 # 非递归思想，使用循环语句，不断累加
-def sum(start, end):
+def sum1(start, end):
     accumulator = 0
     for n in range(start, end + 1):
         accumulator += n
     return accumulator
 
 
-result = sum(1, 100)
+result = sum1(1, 100)
 print("Non-Recursive:", result)
 
 # 请Python"忘掉"上面那个sum函数
-del sum
+del sum1
 
 # 递归思想，使用递归函数的自然实现
-def sum(start, end):
+
+
+def sum1(start, end):
     if start < end:
-        return start + sum(start+1, end)
+        return start + sum1(start+1, end)
     else:
         return end  # 或return start
 
 
-result = sum(1, 100)
+result = sum1(1, 100)
 print("Recursive:", result)
-
